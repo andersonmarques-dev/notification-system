59c961bd91a790a5b2714ecc57741096d950796c
Anderson Felipe <anderson.marques@edu.unimmontes.br>
2026-06-01 16:57:20 -0300

    feat(worker): cria job de notificacao com roteamento e tratamento de falhas

---
+++ app/Jobs/SendNotificationJob.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use App\Models\NotificationLog;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendNotificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public NotificationLog $log;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(NotificationLog $log)
+    {
+        $this->log = $log;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try{
+            $mailable = match($this->log->event_type) {
+                default => throw new Exception("Tipo de Evento Desconhecido: {$this->log->event_type}"),
+            };
+
+            Mail::to($this->log->recipient)->send($mailable);
+
+            $this->log->update([
+                'status' => 'enviado',
+            ]);
+
+        } catch (Exception $e) {
+            $this->log->update([
+                'status' => 'failed',
+                'error_message' => $e->getMessage(),
+            ]);
+
+            throw $e;
+        }
+    }
+}
